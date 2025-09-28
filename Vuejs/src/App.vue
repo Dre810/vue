d7b97e07fbbd1c7c3c977b3a6910d3bd4b64eccf
Davis Kamau <daviskamau810@gmail.com>
2025-09-28 13:59:16 +0300

    initial commit

---
+++ Vuejs/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+
+      <button @click="alertMessage">Read More</button>
+  </div>
+
+  <!-- main comp will be here -->
+  <MainComp/>
+</template>
+
+<script>
+
+import MainComp from './components/MainComp.vue';
+
+export default {
+  //properties
+  data(){
+    return{
+      title:"Hello world",
+      description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate iure tempora optio dolorum non, earum ab aliquam fugiat illo laudantium cumque excepturi expedita ut. Voluptatum quasi perspiciatis molestiae impedit incidunt.",
+    };
+  },
+  //methods
+  methods:{
+    alertMessage() {
+      alert("Hello, you clicked on readmore");
+    },
+  },
+
+  components: {
+    MainComp,
+  }
+};
+</script>
+
+<style scoped>
+h1{
+  color: blue;
+}
+.header{
+  background-color: chartreuse;
+  padding: 10px;
+  text-align: center;
+}
+</style>
---
+++ Vuejs/src/components/MainComp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Hello welcome to the main section of our website</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MainComp"
+}
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
